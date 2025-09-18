05996458af84583cc9fdd8d24f60db420b01a330
Fall31 <lazomorenowilsonlazo@gmail.com>
2025-09-18 19:02:35 -0400

    Crear dbcontext y modificar program.cs

---
+++ MVCPedidos/Data/PedidosDBContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCPedidos.Models;
+namespace MVCPedidos.Data
+{
+    public class PedidosDBContext: DbContext
+    {
+        public PedidosDBContext(DbContextOptions<PedidosDBContext> options) : base(options) { }
+        public DbSet<UsuarioModel> Usuario { get; set; }
+    }
+}
