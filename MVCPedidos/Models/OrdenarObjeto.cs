d8b9a59497482bf6c47214a95352c821eea65e2b
Fall31 <lazomorenowilsonlazo@gmail.com>
2025-09-18 19:32:44 -0400

    Crear modelo ordenar objeto

---
+++ MVCPedidos/Models/OrdenarObjeto.cs
@@ -0,0 +1,10 @@
+﻿namespace MVCPedidos.Models
+{
+    public class OrdenarObjeto
+    {
+        public int Id { get; set; }
+        public string Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
