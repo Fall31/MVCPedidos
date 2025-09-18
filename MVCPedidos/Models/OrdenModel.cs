00efd36afaf0ba465371b4ffc6acb011298b81f9
Fall31 <lazomorenowilsonlazo@gmail.com>
2025-09-18 19:23:52 -0400

    Crear modelo orden

---
+++ MVCPedidos/Models/OrdenModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MVCPedidos.Models
+{
+    public class OrdenModel
+    {
+        public int Id { get; set; }
+        public string Cliente { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Estado { get; set; }
+        public decimal Total { get; set; }
+    }
+}
